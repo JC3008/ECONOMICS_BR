7df55899a71a6de3e5229c80e93713e4f2484320
jc <josecarlos.ico@gmail.com>
2022-07-08 09:32:48 -0300

    save_csv_curated developed

--- connections.py
+++ connections.py
@@ -1,9 +1,9 @@
-class folder(object):    
-    Raw = '/home/jc/airflow/Economics_BR/Raw'
-    Staging_Files = '/home/jc/airflow/Economics_BR/Staging_Files'
-
-class url_string(object):
-    fundamentus = 'https://www.fundamentus.com.br/resultado.php'
+class string(object):    
+    folder_raw = '/home/jc/airflow/Economics_BR/Raw'
+    folder_staging_Files = '/home/jc/airflow/Economics_BR/Staging_Files'
+    folder_curated = '/home/jc/airflow/Economics_BR/Curated'
+    url_fundamentus = 'https://www.fundamentus.com.br/resultado.php'
+    cnx_string = 'postgresql://postgres:postgres@127.0.0.1/teste_airflow'  
     
 
 
--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def extract_html():
     Aqui o html é baixado do site fundamentus.com
     '''
     
-    url = url_string.fundamentus
+    url = string.url_fundamentus
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
     req = Request(url, headers = headers)
@@ -30,7 +30,7 @@ def extract_html():
 
     soup = BeautifulSoup(html, 'html.parser')
     soup
-    with open(f"{folder.Raw}/output.html", "w", encoding='utf-8') as file:
+    with open(f"{string.folder_raw}/output.html", "w", encoding='utf-8') as file:
         file.write(str(soup))
 
 def parsing_html():
@@ -38,13 +38,13 @@ def parsing_html():
     Here the html is stored into a pandas dataframe
     Aqui o arquivo html é carregado para o dataframe
     '''
-    url = url_string.fundamentus
+    url = string.url_fundamentus
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
     req = Request(url, headers = headers)
     response = urlopen(req)
     html = response.read()
 
-    soup = open(f"{folder.Raw}/output.html", "r")
+    soup = open(f"{string.folder_raw}/output.html", "r")
     soup = BeautifulSoup(html, 'html.parser')
     colunas_names = [col.getText() for col in soup.find('table', {'id': 'resultado'}).find('thead').findAll('th')]
     colunas = {i: col.getText() for i, col in enumerate(soup.find('table', {'id': 'resultado'}).find('thead').findAll('th'))}
@@ -55,16 +55,16 @@ def parsing_html():
         linha = soup.find('table', {'id': 'resultado'}).find('tbody').findAll('tr')[i].getText().split('\n')[1:]
         inserir_linha = pd.DataFrame(linha).T.rename(columns=colunas)
         dados = pd.concat([dados, inserir_linha], ignore_index=True)
-    dados.to_csv(f'{folder.Raw}/raw_fundamentus.csv',sep=';')
+    dados.to_csv(f'{string.folder_raw}/raw_fundamentus.csv',sep=';')
 
 def rename_and_insert_loaded_date():
     '''
-    Renaming columns and assiging the loaded_date new column
-    renomeando colunas e inserindo a coluna loaded_date
+    Renaming columns, assigning the loaded_date new column and saving the previous file as historic
+    renomeando colunas e inserindo a coluna loaded_date e guardando o arquivo como historico
     '''
     timestr = dt.today().strftime('%Y-%m-%d')
-    dado = pd.read_csv(f'{folder.Raw}/raw_fundamentus.csv',sep=';')
-    dado.to_csv(f'{folder.Raw}/hist/hist_{timestr}_raw_fundamentus.csv',sep=';',index=False)
+    dado = pd.read_csv(f'{string.folder_Raw}/raw_fundamentus.csv',sep=';')
+    dado.to_csv(f'{string.folder_Raw}/hist/hist_{timestr}_raw_fundamentus.csv',sep=';',index=False)
     dado.rename(columns={'Cotação':'Cotacao',
     'P/L':'PL',
     'P/VP':'PVP',
@@ -86,10 +86,83 @@ def rename_and_insert_loaded_date():
     time = dt.now()    
     dado['LOADED_DATE'] = time
     dado = dado[['Papel','Cotacao','PL','PVP','PSR','DivYield','P_Ativo','P_CapitalGiro','P_Ebit','P_Ativ_Circ_Liq','Ev_Ebit','Ev_Ebitda','Mrg_Ebit','MrgLiq','LiqCorr','ROIC','ROE','Liq2meses','PatrimLiq','DivBrutaPatrimonio','CrescReceita5anos','LOADED_DATE']]
-    dado.to_csv(f'{folder.Raw}/raw_fundamentus.csv',sep=';',index=False)
+    dado.to_csv(f'{string.folder_Raw}/raw_fundamentus.csv',sep=';',index=False)
 
 
+def save_csv_curated():
 
+    query_silver = '''
+select 
+b."pk_empresas" as "fk_empresas",
+b."cod_listagem" as "cod_listagem",
+a."LOADED_DATE",
+a."Papel",
+a."Cotacao",
+a."PL",
+a."PVP",
+a."PSR",
+a."DivYield",
+a."P_Ativo",
+a."P_CapitalGiro",
+a."P_Ebit",
+a."P_Ativ_Circ_Liq",
+a."Ev_Ebit",
+a."Ev_Ebitda",
+a."Mrg_Ebit",
+a."MrgLiq",
+a."LiqCorr",
+a."ROIC",
+a."ROE",
+a."Liq2meses",
+a."PatrimLiq",
+a."DivBrutaPatrimonio",
+a."CrescReceita5anos"
+
+FROM bronze_fundamentus a
+left join dim_empresas b on a."Papel" = b."ticker"
+'''
+
+    conn_string = string.cnx_string
+    db = create_engine(conn_string)
+    conn = db.connect()
+    dados = pd.read_sql(query_silver,conn)
+    dados['date'] = dados['LOADED_DATE'].dt.date
+    time = dt.now()
+    dados['silver_timestamp'] = time
+    
+    dados['fk_empresas'] = dados['fk_empresas'].fillna(999999)
+    dados['fk_empresas'] = dados['fk_empresas'].astype('int')
+    
+    dados['DivYield'] = dados['DivYield'].apply(lambda x: float(str(x).replace('%','').replace('.','').replace(',','.'))/100)
+    dados['ROIC'] = dados['ROIC'].apply(lambda x: float(str(x).replace('%','').replace('.','').replace(',','.'))/100)
+    dados['ROE'] = dados['ROE'].apply(lambda x: float(str(x).replace('%','').replace('.','').replace(',','.'))/100)
+    dados['CrescReceita5anos'] = dados['CrescReceita5anos'].apply(lambda x: float(str(x).replace('%','').replace('.','').replace(',','.'))/100)
+
+    dados['Cotacao'] = dados['Cotacao'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['PL'] = dados['PL'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['PVP'] = dados['PVP'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['PSR'] = dados['PSR'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['P_Ativo'] = dados['P_Ativo'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['P_CapitalGiro'] = dados['P_CapitalGiro'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['P_Ebit'] = dados['P_Ebit'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['P_Ativ_Circ_Liq'] = dados['P_Ativ_Circ_Liq'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['Ev_Ebit'] = dados['P_Ativ_Circ_Liq'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['Ev_Ebitda'] = dados['P_Ativ_Circ_Liq'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['Mrg Ebit'] = dados['P_Ativ_Circ_Liq'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['MrgLiq'] = dados['P_Ativ_Circ_Liq'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['LiqCorr'] = dados['LiqCorr'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['Liq2meses'] = dados['Liq2meses'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['PatrimLiq'] = dados['PatrimLiq'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+    dados['DivBrutaPatrimonio'] = dados['DivBrutaPatrimonio'].apply(lambda x: float(str(str(x).replace('.','')).replace(',','.')))
+
+    dados = dados[['fk_empresas','cod_listagem','date','LOADED_DATE','silver_timestamp','Papel', 'Cotacao', 'PL', 'PVP', 'PSR', 'DivYield', 'P_Ativo',
+    'P_CapitalGiro', 'P_Ebit', 'P_Ativ_Circ_Liq', 'Ev_Ebit', 'Ev_Ebitda',
+    'Mrg_Ebit','Mrg_Ebit', 'MrgLiq', 'LiqCorr', 'ROIC', 'ROE', 'Liq2meses',
+    'PatrimLiq', 'DivBrutaPatrimonio', 'CrescReceita5anos']]
+
+    dados.to_csv(f'{string.folder_curated}/silver_fundamentus.csv',sep=';',index=False)
+
+save_csv_curated()
 
 
 
