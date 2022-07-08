dd32a0caf489911e71c2593b6df89a85d63f298b
jc <josecarlos.ico@gmail.com>
2022-07-07 21:58:16 -0300

    rename_and_insert_loaded_date developed

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from connections import *
 def extract_html():
     '''
     This piece of code perform the download of html file from fundamentus.com table
+    Aqui o html é baixado do site fundamentus.com
     '''
     
     url = url_string.fundamentus
@@ -35,6 +36,7 @@ def extract_html():
 def parsing_html():
     '''
     Here the html is stored into a pandas dataframe
+    Aqui o arquivo html é carregado para o dataframe
     '''
     url = url_string.fundamentus
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
@@ -55,5 +57,39 @@ def parsing_html():
         dados = pd.concat([dados, inserir_linha], ignore_index=True)
     dados.to_csv(f'{folder.Raw}/raw_fundamentus.csv',sep=';')
 
+def rename_and_insert_loaded_date():
+    '''
+    Renaming columns and assiging the loaded_date new column
+    renomeando colunas e inserindo a coluna loaded_date
+    '''
+    timestr = dt.today().strftime('%Y-%m-%d')
+    dado = pd.read_csv(f'{folder.Raw}/raw_fundamentus.csv',sep=';')
+    dado.to_csv(f'{folder.Raw}/hist/hist_{timestr}_raw_fundamentus.csv',sep=';',index=False)
+    dado.rename(columns={'Cotação':'Cotacao',
+    'P/L':'PL',
+    'P/VP':'PVP',
+    'Div.Yield':'DivYield',
+    'P/Ativo':'P_Ativo',
+    'P/Cap.Giro':'P_CapitalGiro',
+    'P/EBIT':'P_Ebit',
+    'P/Ativ Circ.Liq':'P_Ativ_Circ_Liq',
+    'EV/EBIT':'Ev_Ebit',
+    'EV/EBITDA':'Ev_Ebitda',
+    'Mrg Ebit' : 'Mrg_Ebit',
+    'Mrg. Líq.':'MrgLiq',
+    'Liq. Corr.':'LiqCorr',
+    'Liq.2meses':'Liq2meses',
+    'Patrim. Líq':'PatrimLiq',
+    'Dív.Brut/ Patrim.':'DivBrutaPatrimonio',
+    'Cresc. Rec.5a':'CrescReceita5anos'}
+    , inplace = True)
+    time = dt.now()    
+    dado['LOADED_DATE'] = time
+    dado = dado[['Papel','Cotacao','PL','PVP','PSR','DivYield','P_Ativo','P_CapitalGiro','P_Ebit','P_Ativ_Circ_Liq','Ev_Ebit','Ev_Ebitda','Mrg_Ebit','MrgLiq','LiqCorr','ROIC','ROE','Liq2meses','PatrimLiq','DivBrutaPatrimonio','CrescReceita5anos','LOADED_DATE']]
+    dado.to_csv(f'{folder.Raw}/raw_fundamentus.csv',sep=';',index=False)
 
-parsing_html()
+
+
+
+
+
